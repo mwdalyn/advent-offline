628cd3a640d0f55f8e03a2253ea6d674912ea68a
Maddie <64292760+m-abrev@users.noreply.github.com>
2026-01-27 18:57:58 -0500

    Create current scrape method including completion tracking

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
-advent-py
-# Advent of Code in Python
-*Attempted solutions for Advent of Code 2019-2021, done in Python.*
+advent-offline
+# Advent of Code Offline 
+*Logging personal Advent of Code progress through 2019-2024, in Python.*
+Started thinking about this in 2021 for 2019-2021. Revisited in 2026. 
 
-## Completion Score
-2019: 
+## Goal
+- Bring AOC content and inputs offline.
+- Study puzzles and puzzle structure.
+- Keep a tally of "completed" puzzles.
 
-2020: 
+## Future
+> e.g. additional learning
+- Create a Streamlit dashboard for my current completion score. 
+- Attempt to organize puzzles into groups and study those. 
 
-2021: TBD
 
---------------
+
--- scrape.py
+++ scrape.py
@@ -2,33 +2,86 @@
 This was a file to scrape in bulk all inputs and puzzle premises for past years.
 Wanted to take it offline. Should only be run once. Pauses are built in. 
 """
-import os
-import time
-import requests
+import os, re, time, requests
 import numpy as np
+import pandas as pd
+from bs4 import BeautifulSoup
 
-base_url = r'https://adventofcode.com' 
-years = [2019,2020,2021]
+# Parameters
+BASE_URL = r'https://adventofcode.com' 
+YEARS = [2019,2020,2021,2022,2023,2024] # 2022-2024 added January 2026
+HEADERS = {} # Holds session_id and user_agent
 
-with open('aoc_session_id.txt') as f: # Need session_id, lasts 1 year from creation
-    session_id = f.readlines()[0]
+# Functions
+def parse_puzzle_text(response_text):
+	'''Parse out the premise and log if answered or not.'''
+	# Response to soup
+	soup = BeautifulSoup(response_text, "lxml")
+	text = soup.get_text(separator=" ", strip=True)
+	# Trim everything before the first '---' and after the section 'Answer:' (inclusive) 
+	text = "".join(['---',text.split('---', 1)[1]])
+	# Inject newline after each sentence
+	text = re.sub(r'([.!?])\s+', r'\1\n', text)
+	text = re.sub(r':\s+', ':\n', text)
+	# Remove ending or answer (if already submit in browser)
+	if 'Your puzzle answer' in text:
+		answered = 1
+	elif 'Answer:' in text:
+		answered = 0
+	else:
+		answered = 0 # TODO: Throw error for a day if the day contains neither; convention may have changed
+	text = re.split(r'Answer:', text, 1)[0] # Yet unanswered "end" of premise
+	text = re.split(r'Your puzzle answer', text, 1)[0] # Previously answered "end" of premise
+	return text, answered
 
-for year in years:
-    for n in np.arange(1,25,1):
-        print("Starting year " + str(year) + ", day " + str(n) + ".")
-        puzzle = base_url + r'/' + str(year) + r'/day/' + str(n)
+def check_if_answered(response_text):
+	'''If parsing unnecessary, simple function to test if answered.'''
+	# Remove ending or answer (if already submit in browser)
+	if 'Answer:' in response_text:
+		return False
+	return True
 
-        ## If you want to pull the "premise" of the problem as well:
-        # if os.path.exists(str(year) + "/premise/day" + str(n) + ".txt") is False: 
-        #     puzzle_premise = requests.get(puzzle, cookies={'session':session_id})
-        #     with open(str(year)+"/premise/day"+str(n)+".txt", "w", encoding="utf-8") as f:
-        #         f.write(puzzle_premise.text) # Writes puzzle details/instructions to text file
-        #     time.sleep(15) # Courtesy sleep per request of @ericwastl
-        
-        if os.path.exists(str(year) + "/input/day" + str(n) + ".txt") is False: 
-            puzzle_input = requests.get(puzzle + r'/input', cookies={'session':session_id}) # Rejected, need to log in
-            with open(str(year)+"/input/day"+str(n)+".txt", "w", encoding="utf-8") as f:
-                f.write(puzzle_input.text)
-            time.sleep(15) # Courtesy sleep per request of @ericwastl
-        
-        print("Done with day " + str(n) + ", on to the next.")
+if __name__ == "__main__":
+	# Stash answered status
+	completion_grid = []
+	# Open headers
+	with open('aoc_session_id.txt') as f: # Need session_id, lasts 1 year from creation
+		for line in f:
+			key, value = line.strip().split(':', 1)
+			key, value = key.replace("'","").strip(), value.replace("'","").strip()
+			HEADERS[key] = value
+		SESSION_ID = HEADERS['Session-Id'] # Account with AOC; linked to gh
+		USER_AGENT = HEADERS['User-Agent']
+	# Iterate through years of interest (all with 25 puzzles)
+	for year in YEARS:
+		print('Start year: {}'.format(year))
+		# Init
+		if not os.path.isdir(str(year)):
+			os.makedirs(str(year))
+			os.makedirs(os.path.join(str(year),'input'))
+			os.makedirs(os.path.join(str(year),'premise'))
+		# Review days
+		for n in np.arange(1,25,1): # 25 days each year chosen
+			print("Starting year " + str(year) + ", day " + str(n) + ".")
+			puzzle = BASE_URL + r'/' + str(year) + r'/day/' + str(n)
+			# Pull the "premise" of the problem as well
+			puzzle_premise = requests.get(puzzle, cookies={'session':SESSION_ID})
+			puzzle_text, puzzle_answered = parse_puzzle_text(puzzle_premise.text) 
+			# Stash puzzle_text
+			with open(str(year)+"/premise/day"+str(n)+".txt", "w", encoding="utf-8") as f:
+				f.write(puzzle_text) # Writes puzzle details/instructions to text file
+			time.sleep(25) # Courtesy sleep per request of @ericwastl
+			# Log puzzle_answered
+			# TODO: Develop a way to log which puzzles have been answered (separately? is there a better way?)
+			completion_grid.append([year, n, puzzle_answered])
+			# Grab input
+			puzzle_input = requests.get(puzzle + r'/input', cookies={'session':SESSION_ID}) # Rejected, need to log in
+			with open(str(year)+"/input/day"+str(n)+".txt", "w", encoding="utf-8") as f:
+				f.write(puzzle_input.text)
+			time.sleep(25) # Courtesy sleep per request of @ericwastl
+			print("Done with day " + str(n) + ", on to the next.")
+	# Store completion_grid
+	completion_df = pd.DataFrame(completion_grid, columns=['year', 'day', 'complete'])
+	completion_df['updated_on'] = pd.Timestamp.now()
+	completion_df.to_csv('current_completion_grid.csv')
+
